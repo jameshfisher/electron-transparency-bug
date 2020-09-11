565f59c7ab016239b99444a6e461ed52daec44d5
Jim Fisher <jameshfisher@gmail.com>
2020-09-11 16:42:17 +0100

    move in JS

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-const { app, BrowserWindow } = require('electron');
-app.whenReady().then(() => {
+const { app, BrowserWindow, screen, ipcMain } = require('electron');
+
+function sleep() {
+    return new Promise(resolve => setTimeout(resolve, 500));
+}
+
+app.whenReady().then(async () => {
     const window = new BrowserWindow({ transparent: true, frame: false });
     window.loadFile('page.html');
+    await sleep();
+    window.setPosition(-50, -50);
+    await sleep();
+    window.setPosition(50,  50);
 });
